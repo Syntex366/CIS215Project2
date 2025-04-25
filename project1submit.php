2163fe387a4f41e3f790c53fc60470fd17dd6452
Quinton <librock.quinton@gmail.com>
2025-04-25 15:06:07 -0400

    Fix: Unique Emails

--- project1submit.php
+++ project1submit.php
@@ -34,9 +34,7 @@ function validate(){
     # Now, let's make sure the results make sense.
 
     # Email
-    if(!filter_var($_POST["email-name"], FILTER_VALIDATE_EMAIL)){
-        return "Please enter a valid email address.";
-    }
+    
 
     # This next stuff is some complicated SQL commands to determine if there is an email like the one given.
     # equivalent to: select count(email) from project_data where email like "kegross%" and email like "%genesee.edu";
@@ -46,18 +44,22 @@ function validate(){
 
     ## This is the Email validation that doesn't work!
 
-    /* $email = filter_var($_POST["email-name"], FILTER_VALIDATE_EMAIL);
+    $email = filter_var($_POST["email-name"], FILTER_VALIDATE_EMAIL);
+    if(!filter_var($_POST["email-name"], FILTER_VALIDATE_EMAIL)){
+        return "Please enter a valid email address.";
+    }
     $email_pieces = explode("@", $email);
     $front = '"' . $email_pieces[0] . "%" . '"';
     $back = '"' . "%" . $email_pieces[1] . '"';
     global $db;
     $num_emails = $db->prepare("SELECT count(email) FROM project_data where email like $front and email like $back");
     $num_emails->execute();
-    $fetch_emails = $num_emails->fetchAll();
+    $count = $num_emails->fetchColumn();
+    
     # This is getting the size of the array, because all we care about is if it's empty or not
-    if(count($fetch_emails) > 0){
+    if($count > 0){
         return "Only one entry per email.";
-    } */
+    }
 
     # Age
     $age_list = ["0"];
