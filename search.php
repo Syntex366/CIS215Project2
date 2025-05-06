1f5c92c2a0fbc3d05bf7a5d268959f2efae29745
Tyrell <tjwright2023@gmail.com>
2025-05-06 08:15:41 -0400

    added: search.php
    This is the PHP part of my search bar. I was trying to find a way to make the data from the SQL table work for the search bar.

---
+++ search.php
@@ -0,0 +1,23 @@
+<?php
+    header("Content-Type: application/json");
+    include "dbconfig.php";
+
+    $sql = ("SELECT * FROM project_data"); /* prepares all data from the database table to be displayed in an HTML table */
+    $result = $conn->query($sql);
+
+    $data = [];
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    } 
+
+    else {
+        echo json_encode(["error" => "No data found"]);
+        exit;
+    }
+
+    echo json_encode($data);
+    $conn->close();
+?>
